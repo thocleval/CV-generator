de046a968626652f8652ebab412b585313cd6493
Clement Chaumel <clement.chaumel@gmail.com>
2018-06-23 20:18:35 +0100

    Add the new 'Article' template.

--- generate.sh
+++ generate.sh
@@ -4,7 +4,15 @@ if ! type relaxed > /dev/null; then
     echo This program needs relaxed to work
     echo run \'npm i -g relaxedjs\' to install it
 else
-    find ./Templates -name "*cv.pug" -exec relaxed {} --build-once \;
-    mkdir -p PDFs
-    find ./Templates -name "*cv.pdf" -exec mv -f {} ./PDFs \;
+
+    if ! type icon-font-generator > /dev/null; then
+        echo This program needs icon-font-generator to work
+        echo run \'npm install -g icon-font-generator\' to install it
+    else
+        mkdir -p dist
+        icon-font-generator icons/*.svg -o dist
+        find ./Templates -name "*cv.pug" -exec relaxed {} --build-once \;
+        mkdir -p PDFs
+        find ./Templates -name "*cv.pdf" -exec mv -f {} ./PDFs \;
+    fi
 fi
